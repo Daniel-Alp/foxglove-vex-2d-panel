2f115e22c81a90b714af12724c85b00521f6379b
Daniel Alp <daniel.alp.email@gmail.com>
2024-07-01 00:10:19 -0400

    add reset view button

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
 import { drawOnCanvas } from "./renderer";
@@ -22,13 +23,13 @@ export function Canvas({ paths }: { paths: Path[] }): JSX.Element {
   }, [paths, viewCorners]);
 
   function handleOnWheel(e: React.WheelEvent<HTMLCanvasElement>) {
-    const boundingRect = e.currentTarget.getBoundingClientRect();
-    const xCanvas = e.clientX - boundingRect.left;
-    const yCanvas = boundingRect.height - (e.clientY - boundingRect.top);
+    const { width, height, left, top } = e.currentTarget.getBoundingClientRect();
+    const xCanvas = e.clientX - left;
+    const yCanvas = height - (e.clientY - top);
     const { x1, y1, x2, y2 } = viewCorners;
 
-    const xView = linearInterpolate(x1, x2, xCanvas / boundingRect.width);
-    const yView = linearInterpolate(y1, y2, yCanvas / boundingRect.height);
+    const xView = linearInterpolate(x1, x2, xCanvas / width);
+    const yView = linearInterpolate(y1, y2, yCanvas / height);
     const t = Math.sign(e.deltaY) * -0.1;
 
     setViewCorners({
@@ -43,9 +44,9 @@ export function Canvas({ paths }: { paths: Path[] }): JSX.Element {
     if (!dragging) {
       return;
     }
+
     const { width, height } = e.currentTarget.getBoundingClientRect();
     const { x1, y1, x2, y2 } = viewCorners;
-
     const viewMovementX = -e.movementX * ((x2 - x1) / width);
     const viewMovementY = e.movementY * ((y2 - y1) / height);
 
@@ -58,11 +59,12 @@ export function Canvas({ paths }: { paths: Path[] }): JSX.Element {
   }
 
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div style={{ width: "100%", height: "100%", position: "relative" }}>
       <canvas
         style={{
           width: "100%",
           height: "100%",
+          position: "absolute",
           objectFit: "contain",
         }}
         width={720}
@@ -80,6 +82,23 @@ export function Canvas({ paths }: { paths: Path[] }): JSX.Element {
           setDragging(false);
         }}
       />
+      <Button
+        style={{
+          right: "1%",
+          bottom: "1%",
+          position: "absolute",
+          color: "black",
+          fontFamily: "monospace",
+          fontWeight: "bold",
+        }}
+        variant="contained"
+        color="inherit"
+        onClick={() => {
+          setViewCorners({ x1: -72, y1: -72, x2: 72, y2: 72 });
+        }}
+      >
+        Reset view
+      </Button>
     </div>
   );
 }
